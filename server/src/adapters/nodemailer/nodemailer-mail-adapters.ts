9c87c32266414952534ed7a02c42dc8acae2cbb9
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-05-06 12:49:42 -0300

    Refatorando rotas, aplicando envio de email e testes automatizados

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapters.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e5e91848803303",
+    pass: "276ad9b26a759e"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Leonardo Rodrigues <leonardo.t.rodri@gmail.com',
+      subject,
+      html: body,
+    });
+  };
+}
